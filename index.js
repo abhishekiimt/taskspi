1a79c903c997f8a9148cb4f4fc77fe1ca889479f
Abhishek Pandey <abhishekbca15.iimtgn@gmail.com>
2021-08-25 13:14:50 +0530

    update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path")
 const authRoute = require("./routes/Auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/post");
@@ -10,6 +11,10 @@ const postRoute = require("./routes/post");
 //init app
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res)=>{
+  res.sendFile(path.resolve(__dirname) + '/index.html')
+})
 
 //middleware
 app.use(express.json());
